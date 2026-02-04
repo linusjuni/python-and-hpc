97613d693879ee70a95f586f6a60a43276d8cc41
linusjuni <linus@juni.dk>
2026-02-04 11:33:59 +0100

    adds solution for 4

---
+++ exercises/week_1/job_script_4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -q hpc
+#BSUB -W 2
+#BSUB -J multicore_job
+#BSUB -o multicore_%J.out
+#BSUB -e multicore_%J.err
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+
+# -n 4: Request 4 cores
+# -R "span[hosts=1]": All cores on the same node
+
+echo "Running on host: $(hostname)"
+echo "Number of cores allocated: $LSB_DJOB_NUMPROC"
+
+/bin/sleep 60
