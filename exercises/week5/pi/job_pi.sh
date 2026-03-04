e3064ae33b1e6a9405eb87ceecea754b936891b4
linusjuni <linus@juni.dk>
2026-03-04 20:18:10 +0100

    week5

---
+++ exercises/week5/pi/job_pi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#BSUB -q hpc
+#BSUB -n 10
+#BSUB -R "select[model == XeonGold6126]"
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -W 30
+#BSUB -J pi
+#BSUB -o pi_%J.out
+#BSUB -e pi_%J.err
+
+uv sync
+
+echo "=== fully_serial.py ==="
+time uv run python exercises/week5/pi/fully_serial.py
+
+echo "=== fully_parallel.py ==="
+time uv run python exercises/week5/pi/fully_parallel.py
+
+echo "=== chunked_parallel.py ==="
+time uv run python exercises/week5/pi/chunked_parallel.py
