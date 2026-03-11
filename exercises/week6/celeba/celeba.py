1ad2d35278858c0933dd1ca4e186b7c9b27eb5c8
linusjuni <linus@juni.dk>
2026-03-11 10:32:00 +0100

    celeba

---
+++ exercises/week6/celeba/celeba.py
@@ -0,0 +1,41 @@
+import ctypes
+import multiprocessing as mp
+import sys
+from time import perf_counter as time
+import numpy as np
+def init(shared_arr_):
+    global shared_arr
+    shared_arr = shared_arr_
+
+
+def tonumpyarray(mp_arr):
+    return np.frombuffer(mp_arr, dtype='float32')
+
+
+def reduce_step(args):
+    b, e, s, elemshape = args
+    arr = tonumpyarray(shared_arr).reshape((-1,) + elemshape)
+    arr[b] += arr[b + 1]
+
+
+if __name__ == '__main__':
+    n_processes = 4
+    chunk = 2
+
+    # Create shared array
+    data = np.load(sys.argv[1])
+    elemshape = data.shape[1:]
+    shared_arr = mp.RawArray(ctypes.c_float, data.size)
+    arr = tonumpyarray(shared_arr).reshape(data.shape)
+    np.copyto(arr, data)
+    del data
+
+    # Run parallel sum (first step)
+    t = time()
+    pool = mp.Pool(n_processes, initializer=init, initargs=(shared_arr,))
+    pool.map(reduce_step,
+             [(i, i + chunk, 1, elemshape) for i in range(0, len(arr), chunk)],
+             chunksize=1)
+
+    print(time() - t)
+    print(arr.reshape(-1))
