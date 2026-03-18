c8512954a399bd281a50fd5034a1d71ef639da12
linusjuni <linus@juni.dk>
2026-03-18 11:24:12 +0100

    adds jacobi

---
+++ miniproject/src/jacobi.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def jacobi(u: np.ndarray, interior_mask: np.ndarray, max_iter: int, atol: float = 1e-6) -> np.ndarray:
+    u = np.copy(u)
+
+    for i in range(max_iter):
+        # Compute average of left, right, up and down neighbors, see eq. (1)
+        u_new = 0.25 * (u[1:-1, :-2] + u[1:-1, 2:] + u[:-2, 1:-1] + u[2:, 1:-1])
+        u_new_interior = u_new[interior_mask]
+        delta = np.abs(u[1:-1, 1:-1][interior_mask] - u_new_interior).max()
+        u[1:-1, 1:-1][interior_mask] = u_new_interior
+
+        if delta < atol:
+            break
+
+    return u
