b9ba6f05a416b145050a8b90de1e089a8e7662c0
linusjuni <linus@juni.dk>
2026-03-18 11:24:14 +0100

    adds stats

---
+++ miniproject/src/stats.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def summary_stats(u: np.ndarray, interior_mask: np.ndarray) -> dict:
+    u_interior = u[1:-1, 1:-1][interior_mask]
+    mean_temp = u_interior.mean()
+    std_temp = u_interior.std()
+    pct_above_18 = np.sum(u_interior > 18) / u_interior.size * 100
+    pct_below_15 = np.sum(u_interior < 15) / u_interior.size * 100
+    return {
+        "mean_temp": mean_temp,
+        "std_temp": std_temp,
+        "pct_above_18": pct_above_18,
+        "pct_below_15": pct_below_15,
+    }
