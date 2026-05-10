c9fdecd909140f6346c32d36f42b6d05b3a3be88
linusjuni <linus@juni.dk>
2026-05-10 21:44:23 +0200

    handin

---
+++ exercises/week11/handin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#BSUB -q hpc
+#BSUB -J "job_array[1-10]"
+#BSUB -o exercises/week11/job_array_%J_%I.out
+#BSUB -e exercises/week11/job_array_%J_%I.err
+#BSUB -n 1
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -W 1
+
+echo "Job index: $LSB_JOBINDEX"
