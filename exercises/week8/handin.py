63a51c9183d7641ebafeea05e119374c97f77876
linusjuni <linus@juni.dk>
2026-05-10 21:22:03 +0200

    handin

---
+++ exercises/week8/handin.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+
+csv_path = sys.argv[1]
+chunk_size = int(sys.argv[2])
+
+total = 0.0
+for chunk in pd.read_csv(csv_path, chunksize=chunk_size):
+    total += chunk[chunk['parameterId'] == 'precip_past1h']['value'].sum()
+
+print(total)
